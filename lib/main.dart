b1783254f6c27bac05d5f9058b5921bf2a41e088
Fábio Suzarth <fabiosuzarth@hotmail.com>
2020-05-07 21:49:37 -0300

    pexel projeto

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:pixel/screens/home_page.dart';
+import 'package:flutter/material.dart';
+
+main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
